c691a8d52c2746183434e837a4b2127c278ba3c3
Mark Pearyer <marcusp619@gmail.com>
2019-04-23 13:03:13 -0600

    Add folder structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
