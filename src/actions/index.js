9ebe5909a0fe7c5fcfa01287c64da7adbe1c6196
Mark Pearyer <marcusp619@gmail.com>
2019-04-26 09:29:49 -0600

    Add weather actions

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+export const isLoading = bool => ({
+  type: "IS_LOADING",
+  isLoading: bool
+});
+
+export const hasErrored = message => ({
+  type: "HAS_ERRORED",
+  message
+});
+
+export const weatherFetchDataSuccess = weather => ({
+  type: "WEATHER_FETCH_DATA_SUCCESS",
+  weather
+});
